d7772e77b0b1bba9b05507dec1795021702c6027
Bjoern Winkler <bjrnwnklr@gmail.com>
2021-03-09 07:44:08 +0100

    start of random poem generation, naming to be sorted

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from randompoetry import PoemFormRegistry, CorpusRegistry
 
 app = Flask(__name__)
 
 from app import routes
+
+pfr = PoemFormRegistry.from_json('poemforms.json')
+cr = CorpusRegistry()
+
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
 from app import app
 from randompoetry import Poem, PoemFormRegistry, Corpus, CorpusRegistry
+import random
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello world!"
 
-@app.route('/poem')
-def poem():
-    pfr = PoemFormRegistry.from_json('poemforms.json')
+    # randomly select a style and text source
+    poemstyle = random.choice(pfr.poemforms)
+    textcorpus = random.choice(cr.registry)
+    
